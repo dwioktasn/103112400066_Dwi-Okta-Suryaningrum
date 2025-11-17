5cd7bd329cfc3885fb2a1a1420f6d41bdc6a6fee
Dwi Okta Suryaningrum <141700290+dwioktasn@users.noreply.github.com>
2025-11-17 19:54:49 +0700

    Add files via upload

---
+++ Pertemuan7-Modul7/Unguided/unguided1/main.cpp
@@ -0,0 +1,25 @@
+#include "stack.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    Stack S;
+    CreateStack(S);
+
+    cout << "Hello world!" << endl;
+    push(S,3);
+    push(S,4);
+    push(S,8);
+    pop(S);
+    push(S,2);
+    push(S,3);
+    pop(S);
+    push(S,9);
+    printInfo(S);
+    cout<<"balik stack"<<endl;
+    balikStack(S);
+    printInfo(S);
+
+    return 0;
+}
